06dba339af701b5f52ca543dca2158eb708a1081
Mikko Keiho <mikko.keiho@gmail.com>
2018-02-19 22:03:56 +0200

    muutos

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.send('Haloo! Onko jalat lämpimät')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))
